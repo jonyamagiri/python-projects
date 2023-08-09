4d93fde1300c726318328108c156cefa82729181
Joab Nyamagiri <jonyamagiri@gmail.com>
2023-08-09 15:00:58 +0300

    refactor: word_replace

--- word-replace/README.md
+++ word-replace/README.md
@@ -5,10 +5,10 @@
 ```
 ubuntu@ubuntu-HP:~/$ ./app.py
 This is a simple program that helps you replace words
-Enter your sentence: Lorem ipsum is a pseudo-Latin text used in web design, typography, layout, and printing in place of English to emphasise design elements over content. It's also called placeholder (or filler) text.
-You wrote the following: Lorem ipsum is a pseudo-Latin text used in web design, typography, layout, and printing in place of English to emphasise design elements over content. It's also called placeholder (or filler) text.
-Enter the word to replace: text
-Enter the word replacement: words
-Lorem ipsum is a pseudo-Latin words used in web design, typography, layout, and printing in place of English to emphasise design elements over content. It's also called placeholder (or filler) words.
+Enter your sentence: my name is Abel, a figma designer
+You wrote the following: my name is Abel, a figma designer
+Enter the word to replace: figma
+Enter the word replacement: UI/UX
+my name is Abel, a UI/UX designer
 ubuntu@ubuntu-HP:~/$
 ```
--- word-replace/app.py
+++ word-replace/app.py
@@ -1,11 +1,29 @@
 #!/usr/bin/env python3
 
-def replace_word():
-    print('This is a simple program that helps you replace words')
-    str = input("Enter your sentence: ")
-    print('You wrote the following: ' + str)
+def main():
+    print("This is a simple program that helps you replace words")
+    
+    sentence = input("Enter your sentence: ")
+    print("You wrote the following:", sentence)
+    
     word_to_replace = input("Enter the word to replace: ")
     word_replacement = input("Enter the word replacement: ")
-    print(str.replace(word_to_replace, word_replacement))
+    
+    result = sentence.replace(word_to_replace, word_replacement)
+    print(result)
 
-replace_word()
+if __name__ == "__main__":
+    main()
+
+# alternative code that achieves the same results
+# #!/usr/bin/env python3
+
+# def replace_word():
+#     print('This is a simple program that helps you replace words')
+#     str = input("Enter your sentence: ")
+#     print('You wrote the following: ' + str)
+#     word_to_replace = input("Enter the word to replace: ")
+#     word_replacement = input("Enter the word replacement: ")
+#     print(str.replace(word_to_replace, word_replacement))
+
+# replace_word()
